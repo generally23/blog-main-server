2689a6eb3e6c439c24a19a2099eed42d6b0f9f59
fatoumata balde <fatoumatabalde@fatoumatas-MacBook-Pro.local>
2021-10-20 10:27:33 -0400

    initializing account route handlers

---
+++ routeHandlers/account_route_handlers.js
@@ -0,0 +1,21 @@
+const catchAsyncErrors = require('../utils/catch_async_errors');
+
+const signup = catchAsyncErrors((req, res, next) => {});
+
+const signin = catchAsyncErrors((req, res, next) => {});
+
+const signout = catchAsyncErrors((req, res, next) => {});
+
+const changePassword = catchAsyncErrors((req, res, next) => {});
+
+const forgotPassword = catchAsyncErrors((req, res, next) => {});
+
+const updateMyAccount = catchAsyncErrors((req, res, next) => {});
+
+const deleteMyAccount = catchAsyncErrors((req, res, next) => {});
+
+const forgotMyPassword = catchAsyncErrors((req, res, next) => {});
+
+const resetMyPassword = catchAsyncErrors((req, res, next) => {});
+
+const pullMyAccount = catchAsyncErrors((req, res, next) => {});
