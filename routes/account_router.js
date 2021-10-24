641d39f1b0357aec36097b8e7db8d214040e1f29
fatoumata balde <fatoumatabalde@fatoumatas-MacBook-Pro.local>
2021-10-23 20:21:45 -0400

    adding route handlers to routes

--- routes/account_router.js
+++ routes/account_router.js
@@ -1,21 +1,35 @@
 const router = require('express').Router();
 
+const {
+  signup,
+  signin,
+  signout,
+  changeMyPassword,
+  updateMyAccount,
+  deleteMyAccount,
+  forgotMyPassword,
+  resetMyPassword,
+  pullMyAccount
+} = require('../routeHandlers/account_route_handlers');
+
 const parent_route = '/accounts';
 
-router.post(`${parent_route}/signup`);
+router.post(`${parent_route}/signup`, signup);
 
-router.post(`${parent_route}/signin`);
+router.post(`${parent_route}/signin`, signin);
 
-router.post(`${parent_route}/signout`);
+router.post(`${parent_route}/signout`, signout);
 
-router.get(`${parent_route}/my-account`);
+router.get(`${parent_route}/my-account`, pullMyAccount);
 
-router.post(`${parent_route}/forgot-my-password`);
+router.post(`${parent_route}/forgot-my-password`, forgotMyPassword);
 
-router.post(`${parent_route}/reset-password/:reset_token`);
+router.patch(`${parent_route}/reset-password/:reset_token`, resetMyPassword);
 
-router.post(`${parent_route}/change-my-password`);
+router.patch(`${parent_route}/change-my-password`, changeMyPassword);
 
-router.post(`${parent_route}/update-my-account`);
+router.post(`${parent_route}/update-my-account`, updateMyAccount);
 
-router.delete(`${parent_route}/delete-my-account`);
+router.delete(`${parent_route}/delete-my-account`, deleteMyAccount);
+
+module.exports = router;
