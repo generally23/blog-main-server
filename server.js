8dbc07b02eb07339236aebea0995c4b637a4f472
fatoumata balde <fatoumatabalde@fatoumatas-MacBook-Pro.local>
2021-10-23 20:15:02 -0400

    initalizing server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-// imports
+const express = require('express');
+const {
+  connectToDb,
+  setupExpressMiddleware,
+  setuptDotenv,
+  listen
+} = require('./setup');
+
+const server = express();
+
+setuptDotenv();
+connectToDb();
+setupExpressMiddleware(server);
+listen(server);
