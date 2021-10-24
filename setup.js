d4a9d404d95e9c93dc2d4eb848699c19edab02de
fatoumata balde <fatoumatabalde@fatoumatas-MacBook-Pro.local>
2021-10-23 20:14:15 -0400

    initializing server configuration

---
+++ setup.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const postRouter = require('./routes/post_router');
+const accountRouter = require('./routes/account_router');
+
+const setuptDotenv = () => dotenv.config();
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log('suucess connection to db');
+  } catch (error) {
+    console.log('failed db connection');
+  }
+};
+
+const setupExpressMiddleware = server => {
+  // parse json
+  server.use(express.json());
+
+  // server.use(express.static())
+
+  server.use('/blog/v1', accountRouter);
+  server.use('/blog/v1', postRouter);
+};
+
+const listen = async (server, port = process.env.PORT || 80) => {
+  try {
+    await server.listen(port);
+    console.log('listening on port 80');
+  } catch (error) {
+    console.log('failing to listen on port 80');
+  }
+};
+
+module.exports = {
+  connectToDb,
+  setupExpressMiddleware,
+  setuptDotenv,
+  listen
+};
