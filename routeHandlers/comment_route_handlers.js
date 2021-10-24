7a3e16e9e380a892316db1a723fbb17516870285
fatoumata balde <fatoumatabalde@fatoumatas-MacBook-Pro.local>
2021-10-23 20:23:43 -0400

    implementing route handler methods

---
+++ routeHandlers/comment_route_handlers.js
@@ -0,0 +1,7 @@
+const loadPostComments = catchAsyncErrors((req, res, next) => {});
+
+const createPostComment = catchAsyncErrors((req, res, next) => {});
+
+const getCommentsReplies = catchAsyncErrors((req, res, next) => {});
+
+const createCommentReply = catchAsyncErrors((req, res, next) => {});
